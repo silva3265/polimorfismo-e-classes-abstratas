b377e81c732561b1c86fbcbe826d87f1272a6a7d
silva3265 <silva3265@gmail.com>
2024-03-26 14:39:02 -0300

    Downcasting de referências

--- src/com/heranca/banco/CaixaEletronico.java
+++ src/com/heranca/banco/CaixaEletronico.java
@@ -3,8 +3,9 @@ package com.heranca.banco;
 public class CaixaEletronico {
 
 	public static final double TARIFA_TRANSFERENCIA = 10;
+	public static final double TARIFA_IMPRESSAO_DEMONSTRATIVO = 5;
 
-	public void transferir(Conta contaOrigem, Conta contaDestino, double valorTransferencia) { //Conta - supertipo
+	public void transferir(Conta contaOrigem, Conta contaDestino, double valorTransferencia) { // Conta - supertipo
 		System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%d%n", valorTransferencia,
 				contaOrigem.getAgencia(), contaOrigem.getNumero(), contaDestino.getAgencia(), contaDestino.getNumero());
 
@@ -12,12 +13,17 @@ public class CaixaEletronico {
 		contaDestino.depositar(valorTransferencia);
 	}
 
-//	public void transferir(ContaEspecial contaOrigem, ContaSalario contaDestino, double valorTransferencia) {
-//		System.out.printf("Transferindo R$%.2f da conta %d/%d para %d/%d%n", valorTransferencia,
-//				contaOrigem.getAgencia(), contaOrigem.getNumero(), contaDestino.getAgencia(), contaDestino.getNumero());
-//
-//		contaOrigem.sacar(valorTransferencia + TARIFA_TRANSFERENCIA);
-//		contaDestino.depositar(valorTransferencia);
-//	}
+	public void imprimirDemonstrativo(Conta conta) {
+		ContaInvestimento contaInvestimento = (ContaInvestimento) conta; //Downcasting
+
+		if (contaInvestimento.getValorTotalRendimentos() > 0) {
+			System.out.println("Impressão do demonstrativo é gratuita");
+		} else {
+			System.out.printf("Custo da impressão: R$%.2f%n", TARIFA_IMPRESSAO_DEMONSTRATIVO);
+			conta.sacar(TARIFA_IMPRESSAO_DEMONSTRATIVO);
+		}
+
+		conta.imprimirDemonstrativo();
+	}
 
 }
