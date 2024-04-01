a576090def721fc332e3d4db055bd1ab8e3dd00d
silva3265 <silva3265@gmail.com>
2024-04-01 20:30:45 -0300

    Desafio: polimorfismo e classes abstratas

---
+++ src/com/impostos/Pessoa.java
@@ -0,0 +1,17 @@
+package com.impostos;
+
+public abstract class Pessoa {
+
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public abstract double calcularImpostos();
+
+}
